d0cc2f9328852c8480353e4d5aa8fc5fb1a3f2e6
Craig Dent <cgdent@yahoo.com>
2026-03-20 16:20:53 +1100

    Update variables.tf

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -2,6 +2,6 @@ variable "project_name" { default = "acme-webapp-demo" }
 variable "region" { default = "ap-southeast-2" }
 variable "vpc_cidr" { default = "10.0.0.0/16" }
 variable "public_subnet_cidr" { default = "10.0.1.0/24" }
-variable "instance_type" { default = "t2.micro" }
+variable "instance_type" { default = "t2.medium" }
 variable "ami" { default = "ami-0a19dfee4e74708aa" }
 variable "tags" { type = map(string) }
