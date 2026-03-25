32ae655c85410fc947a1af4140aafa65ca5f6412
Craig Dent <cgdent@yahoo.com>
2026-03-25 20:10:42 +1100

    Update main.tf

--- modules/aws-webapp/main.tf
+++ modules/aws-webapp/main.tf
@@ -9,3 +9,24 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch  = true
   tags                     = var.tags
 }
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags   = var.tags
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = var.tags
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
