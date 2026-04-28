f243e694cc7586d849e50ac474d1390427aeb97c
Craig Dent <cgdent@yahoo.com>
2026-04-29 09:40:34 +1000

    Create variables.tf

---
+++ aws/variables.tf
@@ -0,0 +1,7 @@
+variable "project_name" { default = "acme-webapp-demo" }
+variable "region" { default = "ap-southeast-2" }
+variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "public_subnet_cidr" { default = "10.0.1.0/24" }
+variable "instance_type" { default = "t2.micro" }
+variable "ami" { default = "ami-0ac4101c751eae35f" }
+variable "tags" { type = map(string) }
